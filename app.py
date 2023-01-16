f3b163c80ae0e0de0fb41597fa6e25558fa18d73
Roger <roger@rscreativetech.com>
2023-01-16 13:20:38 +1000

    Testing image buffering

--- app.py
+++ app.py
@@ -45,11 +45,10 @@ def inference(model_inputs:dict) -> dict:
     
     # Run the model
     with autocast("cuda"):
-        images = model(prompt, negative_prompt=negative_prompt, height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator).images[0]
-
+        images = model(prompt, negative_prompt=negative_prompt, height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator).images
 
     buffered = BytesIO()
-    images.save(buffered, format="JPEG")
+    images[0].save(buffered, format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     images_base64 = []
